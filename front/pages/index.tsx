7af07a84dc45b83606a1ea2f728992958fe86f46
hisa <vladislav.zavzyatov@mail.ru>
2022-10-02 00:24:24 +0700

    //

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -5,11 +5,19 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div>
-
+    <div className='flex items-center justify-center mt-[20%]'>
+      <div className='text-4xl flex flex-col w-max p-4 border-2 border-black rounded-lg'>
+        <p className='text-center'>authorization</p>
+        <div className='flex items-center mt-2'>
+          <p>login</p>
+          <input className='ml-2' />
+        </div>
+        <div className='flex items-center mt-2'>
+          <p>password</p>
+          <input className='ml-2' />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
