bf4de8070048fb94e8313257d1dafb8e3f6d2307
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-10-02 17:36:40 +0200

    add cors

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException, status
 from services import send_message
 from models import Mail
 from validators import validate_send_mail_request
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post('/send-mail', status_code=status.HTTP_201_CREATED)
 def send_mail(mail:Mail):
     try:
